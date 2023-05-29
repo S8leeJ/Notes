1b2ae1cff32b70b8f423bf5f21d3c66e16a1da07
S8leeJ <jenna.snow.lee@gmail.com>
2023-05-28 19:51:36 -0500

    finish

--- Notes App/js/main.js
+++ Notes App/js/main.js
@@ -1,12 +1,3 @@
-import NotesView from "./NoteView.js"
-
-const app = document.getElementById("app");
-const view  = new NotesView(app, {
-    onNoteAdd(){
-        console.log("Add a new note!");
-    },
-    onNoteEdit(newTitle, newBody){
-        console.log(newTitle);
-        console.log(newBody);
-    }   
-});
+import App from "./App.js";
+const root = document.getElementById("app");
+const app = new App(root);
