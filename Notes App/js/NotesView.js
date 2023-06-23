0b15ad630b5f1fe0e205622653dfb89ee824db8a
S8leeJ <jenna.snow.lee@gmail.com>
2023-06-23 16:32:27 -0500

    awd

--- Notes App/js/NotesView.js
+++ Notes App/js/NotesView.js
@@ -15,7 +15,7 @@ export default class NotesView{
             </div>
             <div class="notes__preview">
                 <input class="notes__title" type="text" placeholder="New Note..">
-                <textarea class="notes__body">Take note...</textarea>
+                <textarea class="notes__body">Takes note...</textarea>
             </div>
         `;
         //click listeners for the buttons
